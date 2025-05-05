b35a23b933a60bc04eb4653b9e27c3d9731b3906
Ermengol B. (Professor) <ebota@inspedralbes.cat>
2025-05-06 01:53:13 +0200

    mongodb i express. falta hpp amb mongo

---
+++ php/mongo.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php';
+
+$client = new MongoDB\Client("mongodb://mico:filos@mongo:27017");
+
+$collection = $client->demo->users;
+
+// Insereix dades d'exemple
+$collection->insertMany([
+    ['name' => 'Anna', 'age' => 28],
+    ['name' => 'Marc', 'age' => 35],
+    ['name' => 'Laia', 'age' => 22]
+]);
+
+echo "Dades inserides correctament.\n";
